1938f2c0fecfba180f5ee675b50f6647021dc0b1
Jess Perry <47005135+jolpica@users.noreply.github.com>
2025-04-21 22:52:23 +0100

    Use cobra

---
+++ cmd/stress.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// stressCmd represents the stress command
+var stressCmd = &cobra.Command{
+	Use:   "stress",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("stress called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stressCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// stressCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// stressCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
